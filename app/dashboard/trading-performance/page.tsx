f0b93d1d8874f8ff3bc923ecc34f7619aef55694
Niacinamide <glasyalabolasaga@gmail.com>
2025-08-05 18:06:59 +0700

    Update page.tsx

--- app/dashboard/trading-performance/page.tsx
+++ app/dashboard/trading-performance/page.tsx
@@ -1,10 +1,8 @@
+"use client";
 
-import { DataTable } from "./data-table";
-import { ClosedPnl, columns } from "./columns";
 import { TableContainer } from "./table-container";
 
-
-export default async function Performance() {
+export default function Performance() {
 
     return (
         <div className="pt-20 px-4 flex flex-col gap-4">
