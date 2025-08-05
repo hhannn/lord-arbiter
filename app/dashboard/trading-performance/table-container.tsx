a7c8ab55b0d79102a9aab7f091565746bed56672
Niacinamide <glasyalabolasaga@gmail.com>
2025-08-05 18:14:01 +0700

    Update table-container.tsx

--- app/dashboard/trading-performance/table-container.tsx
+++ app/dashboard/trading-performance/table-container.tsx
@@ -16,7 +16,7 @@ function renameKeys(obj: Record<string, any>, keyMap: Record<string, string>) {
 
 export function TableContainer() {
     const { data, startPolling, stopPolling } = useUserData();
-    let closedPnl = data.closedPnL?.result.list;
+    let closedPnl = data?.closedPnL?.result.list ?? [];
 
     closedPnl = closedPnl.map((item: any) =>
         renameKeys(item, { symbol: "asset" })
